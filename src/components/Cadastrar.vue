bd36d5cdeeacb3fbbd85aaa00403b776434e70ae
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-24 10:51:24 -0300

    Update Cadastrar.vue

--- src/components/Cadastrar.vue
+++ src/components/Cadastrar.vue
@@ -5,15 +5,15 @@
       <label for="input-small">Nome:</label>
     </b-col>
     <b-col sm="10">
-      <b-form-input id="input-small" size="sm" v-model="nome" placeholder="Digite o seu nome "></b-form-input>
+      <b-form-input  size="sm" v-model="nome" placeholder="Digite o seu nome "></b-form-input>
     </b-col>
   </b-row>
   <b-row class="my-1">
     <b-col sm="2">
-      <label for="input-small">Numero:</label>
+      <label >Numero:</label>
     </b-col>
     <b-col sm="10">
-      <b-form-input id="input-small" size="sm" v-model="numero" placeholder="Digite um numero disponivel de 1 a 100"></b-form-input>
+      <b-form-input  size="sm" v-model="numero" placeholder="Digite um numero disponivel de 1 a 100"></b-form-input>
     </b-col>
   </b-row>
 <div><b-button variant="outline-dark" @click="postar(),reloadPage() ">Cadastrar</b-button>
